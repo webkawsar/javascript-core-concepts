eff9eea30667457b3d04cec511bdefb5502b6031
Kawsar <web.kawsarahmed@gmail.com>
2023-07-19 15:27:52 +0600

    practice constructor function

---
+++ constructor_function.js
@@ -0,0 +1,17 @@
+
+
+// constructor function
+function Cow(name, price, children) {
+    this.name = name;
+    this.price = price;
+    this.children = children;
+}
+
+const cow = new Cow('Friezean Cow', 400000, true);
+console.log(cow)
+
+
+
+
+
+
