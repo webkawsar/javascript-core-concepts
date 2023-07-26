fd6d80f92cdcf7011740327eee0fcc14d6b06281
Kawsar <web.kawsarahmed@gmail.com>
2023-07-26 21:16:48 +0600

    array destructuring

---
+++ 14. Destructring Rest and Spread/array_destructuring.js
@@ -0,0 +1,11 @@
+
+
+const profile = ['Kawsar', 27, 'Web Developer', 'Aggro Farm'];
+
+const [name = 'Unknown', , profession, hobby] = profile;
+console.log(name)
+// console.log(age)
+console.log(profession)
+console.log(hobby)
+
+
---
+++ 14. Destructring Rest and Spread/index.js
@@ -0,0 +1,10 @@
+
+
+// 14. Destructuring, rest and spread
+// 1. Array destructuring
+// 2. 
+
+
+
+
+
