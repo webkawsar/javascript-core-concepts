056cf148fe7cf6310b53be05507738628cb0ec8e
Kawsar <web.kawsarahmed@gmail.com>
2023-07-27 20:39:39 +0600

    destructuring, rest,, spread

--- 14. Destructring Rest and Spread/array_destructuring.js
+++ 14. Destructring Rest and Spread/array_destructuring.js
@@ -1,5 +1,5 @@
 
-
+// array destructuring
 const profile = ['Kawsar', 27, 'Web Developer', 'Aggro Farm'];
 
 const [name = 'Unknown', , profession, hobby] = profile;
@@ -9,3 +9,35 @@ console.log(profession)
 console.log(hobby)
 
 
+// object destructuring
+
+
+
+
+
+// rest
+
+// spread
+
+// rest in function
+function printName(...rest) {
+    // console.log(kawsar)
+    console.log(rest)
+    // return `${kawsar} ${delowar} ${mohammad}`
+}
+
+// eivabe coma separated kore dile rest er value array hoye jabe
+// console.log(printName('Kawsar',  'Delowar', 'Mohammad'))
+
+const names = ['Kawsar',  'Delowar', 'Mohammad'];
+// console.log(printName(names))
+
+
+// eivabe object pass korle ar parameter  rest diye dorle rest er value array er vitore object hoye jabe
+const myProfileObj = {
+    firstName: 'Kawsar',
+    lastName: 'Ahmed',
+    email: 'kawsarahmed.dev@gmail.com',
+    profession: 'Web Developer'
+}
+console.log(printName(myProfileObj))
--- 14. Destructring Rest and Spread/index.js
+++ 14. Destructring Rest and Spread/index.js
@@ -2,9 +2,14 @@
 
 // 14. Destructuring, rest and spread
 // 1. Array destructuring
-// 2. 
-
-
-
+// 2. Object destructuring
+// 3. Rest operator
+// 4. Spread operator
+// 5. Destructuring and rest in function
+// 6. Spread and arguments in function
+// 7. Optional chaining
+// 8. Nullish coalecing operator
+// 9. Nested Object destructuring
+// exercise
 
 
