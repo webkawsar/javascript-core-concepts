c46110e5221746187bb1b49420f668f253816ab7
Kawsar <web.kawsarahmed@gmail.com>
2023-07-28 15:49:46 +0600

    added some articles link for reference

--- 14. Destructring Rest and Spread/array_destructuring.js
+++ 14. Destructring Rest and Spread/array_destructuring.js
@@ -8,9 +8,62 @@ console.log(name)
 console.log(profession)
 console.log(hobby)
 
+const users = [
+    {
+        firstName: 'Kawsar',
+        lastName: 'Ahmed',
+        userName: 'webKawsar',
+        property: {
+            aggro: true,
+            goat: false,
+            counts: [
+                {
+                    gavi: 1,
+                    bachur: 1,
+                    bokna: 1,
+                    total: 3
+                }
+            ]
+        }
+    },
+    {
+        firstName: 'Imran',
+        lastName: 'Ahmed',
+        userName: 'imran'
+    },
+    {
+        firstName: 'Samim',
+        lastName: 'Mohammad',
+        userName: 'samim'
+    }
+
+]
+
+// const [firstUser, secondUser, lastUser] = users;
+// const [firstUser, ...rest] = users;
+const [{ userName, property: { aggro, counts: [{ gavi }] } }, ...rest] = users;
+// console.log(firstUser)
+// console.log(secondUser)
+// console.log(lastUser)
+// console.log(rest)
+console.log(userName)
+console.log(aggro)
+// console.log(counts)
+// console.log(one)
+console.log(gavi)
+
+
 
 // object destructuring
+let cagingIt = {
+    foo: {
+      bar: 'Nick Cage',
+      counts: [1, 2, 3]
+    }
+  };
 
+let { foo: {counts: [ ct1, ct2, ct3 ]} } = cagingIt;
+console.log(ct2);
 
 
 
@@ -41,3 +94,12 @@ const myProfileObj = {
     profession: 'Web Developer'
 }
 console.log(printName(myProfileObj))
+
+
+
+// my research
+
+// https://medium.com/@pyrolistical/destructuring-nested-objects-9dabdd01a3b8
+// https://dmitripavlutin.com/javascript-object-destructuring/
+// https://www.freecodecamp.org/news/javascript-object-destructuring-spread-operator-rest-parameter/
+
