9a92b4e57145aa3e3b7bf8e4e4ee7cdeb4c8b40c
Kawsar <web.kawsarahmed@gmail.com>
2023-07-29 12:01:07 +0600

    practice scope, closure, hoisting

---
+++ 16. Scope closure hoisting/index.js
@@ -0,0 +1,21 @@
+
+
+// 16. Scope, closure and hoisting
+// 1. Understanding scope
+// 2. scope rules for var
+// 3. scope chain
+// 4. closure scope
+// 5. closure practical use
+// 6. more closure
+// 7. introduction to hoisting
+// 8. hoisting in practice
+// 9. iife => immediately invocked function
+
+
+
+
+
+
+
+
+
