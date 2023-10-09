1382ed6d30cac4f433670bb0b570c9ffde60bccc
Kawsar <web.kawsarahmed@gmail.com>
2023-10-09 12:27:19 +0600

    added some info

---
+++ articles/jargon_word/data_type.js
@@ -0,0 +1,22 @@
+
+
+
+জাভাস্ক্রিপ্ট ডাটা টাইপ
+
+
+// ডাটা টাইপ জানার আগে আমাদের জানতে হবে ডাটা কি জিনিস
+
+// A JavaScript variable can hold any type of data.
+
+
+জাভাস্ক্রিপ্টে অনেক অনেক ডাটা টাইপ আছে। সমস্ত ডাটা টাইপগুলোকে যদি আমরা প্রধানত ২টা ক্যাটাগরিতে ভাগ করে ফেলি তাহলে আমাদের বুঝতে সুবিধা হবে। প্রধান ২টা ক্যাটাগরিতে ভাগ করলে হবেঃ 
+
+1. Primitive Data Type
+2. Reference / Complex Data Type
+
+এখন আমরা জানব Primitive Data Type?
+Primitive Data Type: Primitive Data Type হচ্ছে এমন ডাটা যেটাতে Primitive Value থাকে
+
+
+
+
--- articles/playground.js
+++ articles/playground.js
@@ -1,20 +1,19 @@
-let teams = ['Gias', 'Samim', 'Mustakim', 'Ruhi'];
-let profile = {
-    firsName: 'Kawsar',
-    lastName: 'Ahmed',
-    profession: 'Developer'
+
+
+// const result = !10 + '10';
+// console.log(result)
+
+
+
+
+const myObject = {
+    value: 42,
+    getValue: function () {
+        return this.value;
+    }
 }
 
-// array checking
-if(Array.isArray(teams)) {
-    console.log('এটা একটা Array')
-} else {
-    console.log('আমি জানিইইইইইই না!')
-}
+const getValue = myObject.getValue;
+console.log(getValue())
 
-// object checking
-if(typeof profile === 'object' && profile !== null && !Array.isArray(profile)) {
-    console.log('এটা একটা Object')
-} else {
-    console.log('আমি জানিইইইইইই না!')
-}
+
