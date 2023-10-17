8f241a4096de086eff28c32f1793752079a21f9c
Kawsar <web.kawsarahmed@gmail.com>
2023-10-17 19:00:16 +0600

    minor update

--- articleNotes.js
+++ articleNotes.js
@@ -17,4 +17,21 @@
 
 
 
+JavaScript Topics: 
+Data Types 
+How to create an Array 
+How to check is NaN or Not part -1 
+How many ways access Object 
+
+
+Asynchronous, Callback Function, Higher Order Function 
+this 
+Factory function, constructor function 
+Copy by value/copy by reference 
+shallow copy / deep copy from object 
+mutable vs immutable 
+destructuring and spread   
+String.prototype.split() 
+
+what is the properties and method in JavaScript?
 
