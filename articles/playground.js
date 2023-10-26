b8a926ab1af924a3884e1f0c916ed8e25409b795
Kawsar <web.kawsarahmed@gmail.com>
2023-10-26 10:43:05 +0600

    added javascript string prototype split() articles

--- articles/playground.js
+++ articles/playground.js
@@ -1,7 +1,28 @@
-
-
 // const result = !10 + '10';
 // console.log(result)
 
+// case 1
+console.log(0 === -0); // true
+console.log(-0 === +0); // true
+console.log(+0 === -0); // true
+console.log(-0 === -0); // true
 
+// case 2
+console.log(NaN == NaN); // false
+console.log(NaN === NaN); // false
 
+console.log(Object.is(-0, +0));
+
+console.log(Number(+0));
+console.log(Number(-0));
+
+// Object.is() doesn't coerce either value
+// Object.is() and === is in their treatment of signed zeros and NaN values
+
+let text = "How are you doing today?";
+newArray = text.split(",");
+console.log(newArray); // ['How are you doing today?']
+
+text = "How are you doing today?";
+newArray = text.split("@");
+console.log(newArray); // ['How', 'are', 'you']
