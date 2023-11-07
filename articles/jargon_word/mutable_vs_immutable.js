dfa196fab4cda4a34556e6f115f2b1ae8f9cfc04
Kawsar <web.kawsarahed@gmail.com>
2023-11-07 09:32:41 +0600

    mutable vs immutable is ready to post

--- articles/jargon_word/mutable_vs_immutable.js
+++
@@ -1,8 +0,0 @@
-
-
-
-
-/**************************** Reference **************************/
-https://developer.mozilla.org/en-US/docs/Glossary/Mutable
-
-https://www.freecodecamp.org/news/mutability-vs-immutability-in-javascript/#:~:text=Mutability%20refers%20to%20data%20types,still%20access%20in%20the%20memory.
